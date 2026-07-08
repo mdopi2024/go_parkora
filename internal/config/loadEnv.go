b686efcf7d3bd3a942334cb2fe92af528ee888ec
mdopi2024 <you@example.comprogrammeropi2024@gmail.com>
2026-07-08 16:12:02 +0600

    load env file updated

--- internal/config/loadEnv.go
+++ internal/config/loadEnv.go
@@ -1,6 +1,35 @@
+// package config
+
+// import (
+// 	"os"
+
+// 	"github.com/joho/godotenv"
+// )
+
+// type Config struct {
+// 	Port      string
+// 	PG_DSN    string
+// 	SecretKey string
+// }
+
+// func LoadEnv() *Config {
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		panic(err)
+// 	}
+// 	confgoenv := &Config{
+// 		Port:      os.Getenv("PORT"),
+// 		PG_DSN:    os.Getenv("PG_DSN"),
+// 		SecretKey: os.Getenv("SECRET_KEY"),
+// 	}
+
+// 	return confgoenv
+// }
+
 package config
 
 import (
+	"log"
 	"os"
 
 	"github.com/joho/godotenv"
@@ -13,15 +42,14 @@ type Config struct {
 }
 
 func LoadEnv() *Config {
-	err := godotenv.Load()
-	if err != nil {
-		panic(err)
+	// Try to load .env file (for local development only)
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using environment variables")
 	}
-	confgoenv := &Config{
+
+	return &Config{
 		Port:      os.Getenv("PORT"),
 		PG_DSN:    os.Getenv("PG_DSN"),
 		SecretKey: os.Getenv("SECRET_KEY"),
 	}
-
-	return confgoenv
 }
